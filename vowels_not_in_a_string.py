a1e18671245ced4ffc79bb104da37bd9e587cd9b
k.varshini <97451485+21mh1a05a4@users.noreply.github.com>
2022-06-30 20:54:45 +0530

    a=input() b="" c='aeiou' s=0 for i in a:     if i in 'aeiouAEIOU':         if i not in b:             b+=i for i in c:     if i not in b:         print(i,end=' ')         s=1 if s==0:     print('0')

---
+++ vowels_not_in_a_string.py
@@ -0,0 +1,14 @@
+a=input()
+b=""
+c='aeiou'
+s=0
+for i in a:
+    if i in 'aeiouAEIOU':
+        if i not in b:
+            b+=i
+for i in c:
+    if i not in b:
+        print(i,end=' ')
+        s=1
+if s==0:
+    print('0')
