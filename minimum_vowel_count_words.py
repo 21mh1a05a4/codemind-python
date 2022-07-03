08da6c57e5619f1c7d58c2dd150e00aa75f53669
k.varshini <97451485+21mh1a05a4@users.noreply.github.com>
2022-07-03 23:29:19 +0530

    a=input() arr=list(a.split()) vow=list("aeiouAEIOU") ma=100 for i in arr:     c=0     for j in i:         if j in vow:             c+=1     if ma>c:         ma=c wc=0 for i in arr:     c=0     for j in i:         if j in vow:             c+=1     if c==ma:         wc+=1 print(wc)

--- minimum_vowel_count_words.py
+++ minimum_vowel_count_words.py
@@ -1,16 +1,20 @@
 a=input()
-a=a.split()
-b=''
-c=0
-for i in a:
-    i=str(i)
-    d=0
+arr=list(a.split())
+vow=list("aeiouAEIOU")
+ma=100
+for i in arr:
+    c=0
     for j in i:
-        if j in 'aeiouAEIOU':
-            d+=1
-    b+=str(d)
-for i in b:
-    if i==min(b):
-        c+=1
-print(c)        
-            
+        if j in vow:
+            c+=1
+    if ma>c:
+        ma=c
+wc=0
+for i in arr:
+    c=0
+    for j in i:
+        if j in vow:
+            c+=1
+    if c==ma:
+        wc+=1
+print(wc)
